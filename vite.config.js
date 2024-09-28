e0ec5bf61e53a970f89c55cc0d61de43da9173f9
Alston <za78357011@gmail.com>
2024-09-28 15:01:13 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2024-W-6/',
+  base: '/Hong-Kong-Wanderlus/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
